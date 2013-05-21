b1710d01dbcd05a3ef6fe84c4605a2646edf8354
Onyemaechi Okafor <evenmatrix@gmail.com>
2013-05-21 20:38:35 +0100

    updated

--- app/controllers/api/v1/tokens_controller.rb
+++ app/controllers/api/v1/tokens_controller.rb
@@ -347,7 +347,7 @@ def interswitch_notify
                 end
                 logger.info"JSON #{json}"
                 request = Typhoeus::Request.new(
-                    "http://localhost:8080/notify",
+                    "http://push.poploda.com/notify",
                     method:        :post,
                     body:          json,
                     params:        {phone_number: @order.user.phone_number}
