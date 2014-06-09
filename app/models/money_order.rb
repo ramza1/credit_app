179c0a45797c570bc433eb237bad318929d6694f
Uchenna Okafor <ramzauchenna@gmail.com>
2014-06-09 13:55:06 +0100

    updated

--- app/models/money_order.rb
+++ app/models/money_order.rb
@@ -1,6 +1,6 @@
 class MoneyOrder < Order
   TRANSACTION_FEE = 0.015
-  validates_numericality_of :amount,:greater_than_or_equal_to=>500,:less_than_or_equal_to=>100000, :only_integer => true ,:message => 'Enter a valid amount between 500 and 100000.it must be a whole number!'
+  validates_numericality_of :amount,:greater_than_or_equal_to=> 100, :message => 'Enter a valid amount greater than 100'
   attr_accessible :item
 
   def name
